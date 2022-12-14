e12fa7a7737eb20432d0b19e39bd206d9e5629c8
rmgaray <ramiro.sgaray@gmail.com>
2022-12-14 15:07:11 -0300

    check that close action has no failed entries

--- frontend/test/Unit/Admin/Close.purs
+++ frontend/test/Unit/Admin/Close.purs
@@ -2,15 +2,23 @@ module Test.Unit.Admin.Close (test) where
 
 import Prelude
 
+import Contract.Monad (throwContractError)
 import Contract.Test.Plutip (PlutipTest, withKeyWallet)
+import Data.Array as Array
 import Test.Common (getAdminWallet, testInitialParamsNoTimeChecks, withWalletsAndPool)
 import Types (ScriptVersion(..))
 import UnbondedStaking.ClosePool (closeUnbondedPoolContract)
 import Utils (nat)
 
+scriptVersion :: ScriptVersion
+scriptVersion = DebugNoTimeChecks
+
 test :: PlutipTest
 test = withWalletsAndPool testInitialParamsNoTimeChecks [] \wallets ubp -> do
     adminWallet <- getAdminWallet wallets
     withKeyWallet adminWallet do
-        _ <- closeUnbondedPoolContract ubp DebugNoTimeChecks (nat 0) []
+        failedIndices <- closeUnbondedPoolContract ubp scriptVersion (nat 0) []
+        -- Make sure that return value is empty list
+        when (not $ Array.null failedIndices) $
+           throwContractError "Some entries failed to be updated"
         pure unit
