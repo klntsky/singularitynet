cda4aaaa8851f1862856173fee04a04dc46d693d
rmgaray <ramiro.sgaray@gmail.com>
2023-01-08 12:30:09 -0300

    add support for recovery in DepositPool action

--- frontend/src/UnbondedStaking/DepositPool.purs
+++ frontend/src/UnbondedStaking/DepositPool.purs
@@ -41,9 +41,12 @@ import Contract.Utxos (utxosAt)
 import Contract.Value (mkTokenName, singleton)
 import Control.Applicative (unless)
 import Ctl.Internal.Plutus.Conversion (fromPlutusAddress)
+import Data.Array ((:))
 import Data.Array as Array
+import Data.Array.NonEmpty as NonEmpty
 import Data.BigInt (BigInt)
 import Data.Unfoldable (none)
+import Partial.Unsafe (unsafePartial)
 import Scripts.PoolValidator (mkUnbondedPoolValidator)
 import Settings
   ( unbondedStakingTokenName
@@ -53,6 +56,7 @@ import Settings
 import Types (ScriptVersion)
 import UnbondedStaking.Types
   ( Entry(Entry)
+  , IncompleteDeposit(..)
   , UnbondedPoolParams(UnbondedPoolParams)
   , UnbondedStakingAction(AdminAct)
   , UnbondedStakingDatum(AssetDatum, EntryDatum, StateDatum)
@@ -73,18 +77,17 @@ import Utils
   )
 
 -- | Deposits a certain amount in the pool
--- | If the `batchSize` is zero, then funds will be deposited to all users.
--- | Otherwise the transactions will be made in batches
--- | If the `depositList` is empty, then reward deposits will be made to all
--- | users. Otherwise only the users within in the list will have rewards
--- | deposited.
+-- If the `batchSize` is zero, then funds will be deposited to all users.
+-- Otherwise the transactions will be made in batches
+-- If `IncompleteDeposit` is provided, then reward deposits will be made only
+-- users whose entries are outdated since the last cycle.
 depositUnbondedPoolContract
   :: BigInt
   -> UnbondedPoolParams
   -> ScriptVersion
   -> Natural
-  -> Array Int
-  -> Contract () (Array Int)
+  -> Maybe IncompleteDeposit
+  -> Contract () (Maybe IncompleteDeposit)
 depositUnbondedPoolContract
   depositAmt
   params@
@@ -96,7 +99,7 @@ depositUnbondedPoolContract
     )
   scriptVersion
   batchSize
-  _depositList = do
+  incompleteDeposit' = do
   -- Fetch information related to the pool
   -- Get network ID and check admin's PKH
   networkId <- getNetworkId
@@ -154,23 +157,48 @@ depositUnbondedPoolContract
         \StateDatum { maybeEntryName: Just ..., open: true }"
 
       -- Obtain the on-chain association list. Note that if a previous deposit
-      -- failed, some of these entries might be updated while the others are not.
+      -- failed, some of these entries might be up to date while others are not.
       let
+        assocList
+          :: Array
+               (ByteArray /\ TransactionInput /\ TransactionOutputWithRefScript)
         assocList = mkOnchainAssocList assocListCs unbondedPoolUtxos
 
-        entriesInputs :: Array TransactionInput
-        entriesInputs = map (fst <<< snd) assocList
-
-      -- Get datums for entries in the list
-      entriesDatums :: Array Entry <- getListDatums assocList
-
-      -- Assign rewards and update fields for next cycle
-      let
-        updatedEntriesDatums :: Array Entry
-        updatedEntriesDatums = updateEntriesList depositAmt entriesDatums
+      -- Obtain the entries' inputs and datums and generate the updated versions
+      -- of the datums.
+      -- If an `IncompleteDeposit` value is provided, only the given
+      -- subset of the entries is updated.
+      (entriesInputs /\ entriesDatums /\ updatedEntriesDatums) <-
+        case incompleteDeposit' of
+          Nothing -> do
+            -- Use all entries
+            let
+              entriesInputs :: Array TransactionInput
+              entriesInputs = map (fst <<< snd) assocList
+            entriesDatums :: Array Entry <- getListDatums assocList
+            -- Assign rewards and update fields for next cycle
+            let
+              updatedEntriesDatums :: Array Entry
+              updatedEntriesDatums = updateEntriesList depositAmt entriesDatums
+            pure $ entriesInputs /\ entriesDatums /\ updatedEntriesDatums
+          Just (IncompleteDeposit incompDeposit) -> do
+            -- Use only the entries with the given keys
+            let
+              assocList' :: Array (ByteArray /\ TransactionInput /\ TransactionOutputWithRefScript)
+              assocList' = Array.filter
+                (\(ba /\ _) -> Array.elem ba incompDeposit.failedKeys)
+                assocList
+              entriesInputs = map (fst <<< snd) assocList'
+            entriesDatums <- getListDatums assocList'
+            -- Assign rewards and update fields to outdated entries
+            let updatedEntriesDatums =
+                   updateOutdatedEntriesList
+                      incompDeposit.nextDepositAmt
+                      incompDeposit.totalDeposited
+                      entriesDatums
+            pure $ entriesInputs /\ entriesDatums /\ updatedEntriesDatums
 
       -- Generate constraints/lookups for updating each entry
-      -- TODO: Take into account depositList for completing partial updates
       entryUpdates
         :: Array ((TxConstraints Unit Unit) /\ (ScriptLookups PlutusData)) <-
         traverse (updateEntryTx params valHash)
@@ -214,12 +242,25 @@ depositUnbondedPoolContract
         "depositUnbondedPoolContract: Finished updating pool entries. /\
         \Entries with failed updates"
         failedDeposits
-      failedDepositsIndicies <-
-        liftContractM
-          "depositUnbondedPoolContract: Failed to create /\
-          \failedDepositsIndicies list" $
-          traverse (flip Array.elemIndex entryUpdates) failedDeposits
-      pure failedDepositsIndicies
+      -- We obtain the failed entries and return them as part of a
+      -- `IncompleteDeposit` (if there are any)
+      pure do
+        indices <- NonEmpty.fromArray =<< traverse
+          (flip Array.elemIndex entryUpdates)
+          failedDeposits
+        let
+          entries = foldl
+            ( \acc i -> unsafePartial $ Array.unsafeIndex updatedEntriesDatums i
+                : acc
+            )
+            mempty
+            indices
+        firstEntry <- Array.head entries
+        Just $ IncompleteDeposit
+          { failedKeys: map (unwrap >>> _.key) entries
+          , totalDeposited: unwrap >>> _.totalDeposited $ firstEntry
+          , nextDepositAmt: depositAmt
+          }
     -- Other error cases:
     StateDatum { maybeEntryName: Nothing, open: true } ->
       throwContractError
@@ -268,6 +309,24 @@ updateEntriesList
   -> Array Entry
 updateEntriesList nextDepositAmt entries =
   uncurry updateTotalDeposited $ updateRewards nextDepositAmt entries
+
+-- | Updates a list of outdated entries. It uses the given `totalDeposited`
+-- to update the entries. This is in contrast to
+-- `updateEntriesList`, which assumes the array of entries to conform the
+-- entirety of the pool and calculates the `totalDeposited` itself.
+updateOutdatedEntriesList
+  :: BigInt
+  -> BigInt
+  -> Array Entry
+  -> Array Entry
+updateOutdatedEntriesList nextDepositAmt totalDeposited =
+    map \entry@(Entry e) ->
+      Entry $ e {
+        newDeposit = zero,
+        rewards = calculateRewards entry,
+        totalRewards = nextDepositAmt,
+        totalDeposited = totalDeposited
+      }
 
 -- | Assign rewards to each entry, set `newDeposit` to zero, set next
 -- cycle rewards. Sum all deposits/rewards.
