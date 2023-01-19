20bb1545eb3084a608d20d93728bd7196391d4a8
rmgaray <ramiro.sgaray@gmail.com>
2023-01-19 20:06:26 -0300

    incorporate expected result of each command

--- frontend/test/Integration/Arbitrary.purs
+++ frontend/test/Integration/Arbitrary.purs
@@ -2,20 +2,43 @@ module SNet.Test.Integration.Arbitrary (arbitraryInputs) where
 
 import Prelude
 
+import Control.Monad.State (StateT, evalStateT, get, lift, put)
 import Data.Array.NonEmpty as NonEmpty
 import Data.BigInt as BigInt
+import Data.Maybe (Maybe(Nothing))
 import Data.Tuple (uncurry)
-import Test.QuickCheck.Gen (Gen, chooseInt, oneOf, vectorOf)
 import SNet.Test.Integration.Types
-  ( StateMachineInputs(..)
-  , UserCommand(..)
-  , AdminCommand(..)
+  ( AdminCommand(..)
+  , CommandResult(..)
   , InputConfig(..)
+  , StateMachineInputs(..)
+  , UserCommand(..)
+  , UserCommand'
+  , AdminCommand'
   )
+import Test.QuickCheck.Gen (Gen, chooseInt, oneOf, vectorOf)
 
-arbitraryUserCommand :: Int -> Int -> Gen UserCommand
-arbitraryUserCommand minStake maxStake =
-  oneOf $ NonEmpty.cons'
+-- | A monad stack that allows us to thread some state between each randomly
+-- generated action. This gives us access to history. Due to how the protocol
+-- is designed, we can tell (for most actions) whether they will succeed or
+-- not only based on the sequence of actions carried by the relevant user.
+type Gen' s a = StateT s Gen a
+
+evalGen' :: forall s a. s -> Gen' s a -> Gen a
+evalGen' s g = evalStateT g s
+
+-- | State used to track which user's actions are valid
+type UserState =
+  { hasStakeInPool :: Boolean
+  }
+
+userState0 :: UserState
+userState0 = { hasStakeInPool: false }
+
+arbitraryUserCommand :: Int -> Int -> Gen' UserState UserCommand'
+arbitraryUserCommand minStake maxStake = do
+  -- We choose randomly among all actions
+  command <- lift $ oneOf $ NonEmpty.cons'
     ( UserStake <<< BigInt.fromInt <$> chooseInt
         minStake
         maxStake
@@ -23,23 +46,56 @@ arbitraryUserCommand minStake maxStake =
     [ pure UserWithdraw
     , pure DoNothing
     ]
+  {- We assign the result of the command based on whether the user has stake
+     in the pool or not.
 
-arbitraryAdminCommand :: Int -> Int -> Gen AdminCommand
-arbitraryAdminCommand minDeposit maxDeposit = oneOf $ NonEmpty.cons'
-  (AdminDeposit <<< BigInt.fromInt <$> chooseInt minDeposit maxDeposit)
-  [ pure <<< AdminDeposit <<< BigInt.fromInt $ 0
-  , pure AdminClose
-  ]
+     1. A user stake always succeeds (unless the amount is out of
+        bounds, but we cannot tell that now).
+     2. A user withdrawal succeeds iff there already is some stake from this
+        user.
+     3. A user doing nothing automatically succeeds.
+
+    We also update the state accordingly.
+  -}
+  s@{ hasStakeInPool } <- get
+  result <- case command of
+    UserStake _ -> put (s { hasStakeInPool = true }) *> pure Success
+    UserWithdraw
+      | hasStakeInPool -> (put s { hasStakeInPool = false }) *> pure Success
+      | otherwise -> pure (Failure Nothing)
+    DoNothing -> pure Success
+
+  pure { command, result }
+
+-- | State used to track which admin's actions are valid
+type AdminState =
+  { hasClosed :: Boolean
+  }
+
+adminState0 :: AdminState
+adminState0 = { hasClosed: false }
+
+arbitraryAdminCommand :: Int -> Int -> Gen' AdminState AdminCommand'
+arbitraryAdminCommand minDeposit maxDeposit = do
+  command <- lift $ oneOf $ NonEmpty.cons'
+    (AdminDeposit <<< BigInt.fromInt <$> chooseInt minDeposit maxDeposit)
+    [ pure <<< AdminDeposit <<< BigInt.fromInt $ 0
+    , pure AdminClose
+    ]
+  let result = Success
+  pure { command, result }
 
 arbitraryInputs :: InputConfig -> Gen StateMachineInputs
 arbitraryInputs (InputConfig cfg) = do
-  userInputs :: Array (Array (Array UserCommand)) <-
-    vectorOf cfg.nCycles
-      $ vectorOf cfg.nUsers do
-          nActions <- chooseInt 1 cfg.maxUserActionsPerCycle
-          vectorOf nActions $
-            uncurry arbitraryUserCommand cfg.stakeRange
-  adminInput :: Array AdminCommand <- vectorOf cfg.nCycles $
-    uncurry arbitraryAdminCommand cfg.depositRange
-  pure $ StateMachineInputs { userInputs, adminInput }
+  usersInputs :: Array (Array (Array UserCommand')) <-
+    vectorOf cfg.nCycles do
+      vectorOf cfg.nUsers do
+        nActions <- chooseInt 1 cfg.maxUserActionsPerCycle
+        vectorOf nActions
+          $ evalGen' userState0
+          $ uncurry arbitraryUserCommand cfg.stakeRange
+  adminInputs :: Array AdminCommand' <- vectorOf cfg.nCycles
+    $ evalGen' adminState0
+    $ uncurry arbitraryAdminCommand cfg.depositRange
+  pure $ StateMachineInputs { usersInputs, adminInputs }
 
