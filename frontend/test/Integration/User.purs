2b235d45a3e9192f31916c3335ccf2618a832dff
rmgaray <ramiro.sgaray@gmail.com>
2023-01-23 17:18:01 -0300

    fix waiting in user withdrawal to support withdrawals after close

--- frontend/test/Integration/User.purs
+++ frontend/test/Integration/User.purs
@@ -5,10 +5,11 @@ import Contract.Prelude
 import Contract.Numeric.Natural as Natural
 import Contract.Test.Plutip (withKeyWallet)
 import Contract.Wallet (KeyWallet)
+import Control.Monad.Error.Class (try)
 import Control.Monad.Reader (ask, lift)
 import Data.BigInt (BigInt)
 import SNet.Test.Common (waitFor)
-import UnbondedStaking.Types (Period(UserPeriod), SnetContract)
+import UnbondedStaking.Types (Period(..), SnetContract)
 import UnbondedStaking.UserStake (userStakeUnbondedPoolContract)
 import UnbondedStaking.UserWithdraw (userWithdrawUnbondedPoolContract)
 
@@ -36,7 +37,7 @@ stake wallet amt = do
 
 withdraw :: KeyWallet -> SnetContract Unit
 withdraw wallet = do
-  waitFor UserPeriod
+  waitForWithdrawal
   { unbondedPoolParams, scriptVersion } <- ask
   lift $ withKeyWallet wallet do
     _txId <- userWithdrawUnbondedPoolContract unbondedPoolParams
@@ -47,3 +48,11 @@ withdraw wallet = do
       * If the pool is closed, the stake should fail and the funds distribution
       should not change.
 -}
+
+-- | We use a wrapper for waitFor' because we don't know exactly what period
+-- to wait for: a withdrawal may happen either in a user or closed period.
+waitForWithdrawal :: SnetContract Unit
+waitForWithdrawal = do
+   result <- try $ waitFor UserPeriod
+   either (const $ waitFor ClosedPeriod) pure result
+
