462fbd675f67364618db7a55ff2a1d7b9b9fe7d8
rmgaray <ramiro.sgaray@gmail.com>
2023-02-27 20:07:09 -0300

    add example for keywallet and NodeJS

--- frontend/ts-example/index.ts
+++ frontend/ts-example/index.ts
@@ -2,7 +2,18 @@ import {
     main
   } from './examples/default';
 
-// Run the contracts, see `main` above
+// Run the contracts, see `main` above.
+//
+// NOTE: If this is run from NodeJS, it is important to explicitly finish the
+// process, since otherwise the process will continue to run after an error is
+// thrown or the program finishes succesfully. This is related to the SDK not
+// properly releasing some resources, which ends up blocking the Node process
+// from exiting.
 main()
-  .then((_) => {})
-  .catch((e) => console.log(e));
+  .then((_) => {
+    //process.exit();
+    })
+  .catch((e) => {
+    console.log(e);
+    //process.exit();
+    }) ;
